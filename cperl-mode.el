qsimpleq/cperl-mode

commit 7b69f6d7c700aa6898a96873a50285980283f25c

Condify at confusing 'if'
I'm not sure if it's the only such case, but it's the one that confused me.

cperl-mode.el
@@ -1208,13 +1208,12 @@ versions of Emacs."
 		      ;;(concat (char-to-string help-char) "v") ; does not work
 		      'cperl-get-help
 		      [(control c) (control h) v]))
-  (if (and (featurep 'xemacs)
-	   (<= emacs-minor-version 11) (<= emacs-major-version 19))
-      (progn
-	;; substitute-key-definition is usefulness-deenhanced...
-	;;;;;(cperl-define-key "\M-q" 'cperl-fill-paragraph)
+  (cond ((and (featurep 'xemacs)
+              (<= emacs-minor-version 11)
+              (<= emacs-major-version 19))
 	(cperl-define-key "\e;" 'cperl-indent-for-comment)
 	(cperl-define-key "\e\C-\\" 'cperl-indent-region))
+        (t
     (or (boundp 'fill-paragraph-function)
 	(substitute-key-definition
 	 'fill-paragraph 'cperl-fill-paragraph

commit 6934da048061a71519fd280934e2e16ac4145062

fix cond parenthesis
fix cond parenthesis, which was broken in 7b69f6d

cperl-mode.el
@@ -1226,9 +1226,9 @@ versions of Emacs."
           cperl-mode-map global-map)
          (substitute-key-definition
           'indent-for-comment 'cperl-indent-for-comment
-          cperl-mode-map global-map))
+          cperl-mode-map global-map))))
 
-        (defvar cperl-menu)))
+        (defvar cperl-menu)
 (defvar cperl-lazy-installed)
 (defvar cperl-old-style nil)
 (condition-case nil